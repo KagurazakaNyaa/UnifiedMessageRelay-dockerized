cb57743c3618b5970f39b8c590f679dfa64d6d98
神楽坂·喵 <yuronglong1996@gmail.com>
2020-05-09 15:28:33 +0800

    fix space

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ![ -f /root/.umr/config.yml ]
+if ! [ -f /root/.umr/config.yml ]
 then
   wget https://raw.githubusercontent.com/JQ-Networks/UnifiedMessageRelay/master/config.yaml -O /root/.umr/config.yml
 fi
